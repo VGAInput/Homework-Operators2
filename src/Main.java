b1741864a46398be96f2cfc200611f4bbc90c760
Иван <ivanpaskhin@yandex.ru>
2022-10-01 21:55:13 +0300

    ДЗ:  Переменные - ч.2 (1-5)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-       lesson1();
-       lesson2();
-       lesson3();
-
+        lesson1();
+        lesson2();
+        lesson3();
+        lesson4();
+        lesson5();
     }
 
     static void lesson1() {
@@ -58,7 +59,51 @@ public class Main {
             System.out.println(year + " год не является високосным.");
     }
 
-    static void lesson4(){
+    static void lesson4() {
+
+        int deliveryDistance = 95;
+        short etaDays = 1;
+
+        if (deliveryDistance > 20)
+            etaDays++;
+        if (deliveryDistance >= 60)
+            etaDays++;
+        if (deliveryDistance >= 100) etaDays++;
+
+        System.out.println("Потребуется дней: " + etaDays);
+
+    }
+
+    static void lesson5() {
+
+        int month = 12;
+
+        switch (month) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Неизвестный месяц!");
+                break;
+        }
 
     }
 
